eadf61772b8b1bd2658da3f6d95603bc37999424
bienfait shomari <bienfaitshm@gmail.com>
2021-11-25 09:02:39 +0200

    prevent action rerender!

--- src/packages/seatings/client.tsx
+++ src/packages/seatings/client.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
 import Box from '@mui/material/Box';
-import Slider, { SliderThumb } from '@mui/material/Slider';
+import Slider from '@mui/material/Slider';
 import { styled } from '@mui/material/styles';
-import Tooltip from '@mui/material/Tooltip';
 
 import { SeatConfigProvider } from './components/SeatConfig';
 import { DescriptionContainer } from './containers/descriptions';
@@ -17,112 +16,64 @@ interface Props {
     actions?: CabineFuncActionType
 }
 
-interface ValueProps {
-    children: React.ReactElement;
-    value: number;
-}
-  
-function ValueLabelComponent(props: ValueProps) {
-    const { children, value } = props;
 
-    return (
-        <Tooltip enterTouchDelay={0} placement="top" title={value}>
-            {children}
-        </Tooltip>
-    );
-}
 
 const iOSBoxShadow =
     '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
 
-    const IOSSlider = styled(Slider)(({ theme }) => ({
-        color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff',
-        height: 2,
-        padding: '15px 0',
-        '& .MuiSlider-thumb': {
-          height: 28,
-          width: 28,
-          backgroundColor: '#fff',
-          boxShadow: iOSBoxShadow,
-          '&:focus, &:hover, &.Mui-active': {
-            boxShadow:
-              '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
-            // Reset on touch devices, it doesn't add specificity
-            '@media (hover: none)': {
-              boxShadow: iOSBoxShadow,
-            },
-          },
+const IOSSlider = styled(Slider)(({ theme }) => ({
+    color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff',
+    height: 2,
+    padding: '15px 0',
+    '& .MuiSlider-thumb': {
+        height: 24,
+        width: 24,
+        backgroundColor: '#fff',
+        border: '2px solid currentColor',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+            boxShadow: iOSBoxShadow,
         },
-        '& .MuiSlider-valueLabel': {
-          fontSize: 12,
-          fontWeight: 'normal',
-          top: -6,
-          backgroundColor: 'unset',
-          color: theme.palette.text.primary,
-          '&:before': {
+        '&:before': {
             display: 'none',
-          },
-          '& *': {
-            background: 'transparent',
-            color: theme.palette.mode === 'dark' ? '#fff' : '#000',
-          },
         },
-        '& .MuiSlider-track': {
-          border: 'none',
+    },
+    '& .MuiSlider-valueLabel': {
+        lineHeight: 1.2,
+        fontSize: 12,
+        background: 'unset',
+        padding: 0,
+        width: 32,
+        height: 32,
+        borderRadius: '50% 50% 50% 0',
+        backgroundColor: '#3880ff',
+        transformOrigin: 'bottom left',
+        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+        '&:before': { display: 'none' },
+        '&.MuiSlider-valueLabelOpen': {
+            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
         },
-        '& .MuiSlider-rail': {
-          opacity: 0.5,
-          backgroundColor: '#bfbfbf',
+        '& > *': {
+            transform: 'rotate(45deg)',
         },
-        '& .MuiSlider-mark': {
-          backgroundColor: '#bfbfbf',
-          height: 8,
-          width: 1,
-          '&.MuiSlider-markActive': {
+    },
+    '& .MuiSlider-track': {
+        border: 'none',
+    },
+    '& .MuiSlider-rail': {
+        opacity: 0.5,
+        backgroundColor: '#bfbfbf',
+    },
+    '& .MuiSlider-mark': {
+        backgroundColor: '#bfbfbf',
+        height: 8,
+        width: 1,
+        '&.MuiSlider-markActive': {
             opacity: 1,
             backgroundColor: 'currentColor',
-          },
         },
-      }));
+    },
+}));
       
-const PrettoSlider = styled(Slider)({
-        color: '#52af77',
-        height: 8,
-        '& .MuiSlider-track': {
-          border: 'none',
-        },
-        '& .MuiSlider-thumb': {
-          height: 24,
-          width: 24,
-          backgroundColor: '#fff',
-          border: '2px solid currentColor',
-          '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-            boxShadow: 'inherit',
-          },
-          '&:before': {
-            display: 'none',
-          },
-        },
-        '& .MuiSlider-valueLabel': {
-          lineHeight: 1.2,
-          fontSize: 12,
-          background: 'unset',
-          padding: 0,
-          width: 32,
-          height: 32,
-          borderRadius: '50% 50% 50% 0',
-          backgroundColor: '#52af77',
-          transformOrigin: 'bottom left',
-          transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-          '&:before': { display: 'none' },
-          '&.MuiSlider-valueLabelOpen': {
-            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-          },
-          '& > *': {
-            transform: 'rotate(45deg)',
-          },
-        },
-});
 
 function valuetext(value: number) {
     return `${value} Bienfait`;
@@ -147,11 +98,20 @@ const marks = [
 export const ClientPlaceReservations = React.forwardRef<any, Props>((props, ref) => {
     const [value, setValue] = React.useState<number[]>([0, marks.length]);
 
-    const handleChange = (event: Event, newValue: number | number[]) => {
-        console.log("bienfait value", newValue)
-        setValue(newValue as number[]);
-    };
-
+    const handleChange = React.useCallback((event: Event, newValue: number | number[]) => {
+        const [nStart, NEnd] = newValue as number[];
+        const [oStart,oEnd] = value;
+        if(nStart !== oStart || NEnd !== oEnd){
+            setValue((state)=>{
+                if(nStart !== NEnd){
+                    return [nStart, NEnd];
+                }
+                return state;
+            });
+        }
+    },[value]);
+    
+    console.log("gloire....", value)
     return (
         <SeatConfigProvider>{(dispatch) => (
             <Grid container spacing={5}>
@@ -162,35 +122,9 @@ export const ClientPlaceReservations = React.forwardRef<any, Props>((props, ref)
                             variant="subtitle2" 
                             marginBottom ={2}
                         >Trajet</Typography>
-                        <Typography gutterBottom>iOS</Typography>
-                            <IOSSlider
-                                valueLabelDisplay="auto"
-                                aria-label="pretto slider"
-                                value={value}
-                                onChange={handleChange}
-                                getAriaValueText={valuetext}
-                                min={0}
-                                max={marks.length-1}
-                                step={null}
-                                marks={marks}
-                            />
-                            <Box sx={{ m: 3 }} />
-                            <Typography gutterBottom>pretto.fr</Typography>
-                            <PrettoSlider
-                                valueLabelDisplay="auto"
-                                aria-label="pretto slider"
-                                value={value}
-                                onChange={handleChange}
-                                getAriaValueText={valuetext}
-                                min={0}
-                                max={marks.length-1}
-                                step={null}
-                                marks={marks}
-                                // valueLabelDisplay="on"
-                            />
-                        {/* <Slider
-                            disableSwap
-                            getAriaLabel={() => 'Temperature range'}
+                        <IOSSlider
+                            valueLabelDisplay="auto"
+                            aria-label="pretto slider"
                             value={value}
                             onChange={handleChange}
                             getAriaValueText={valuetext}
@@ -198,8 +132,9 @@ export const ClientPlaceReservations = React.forwardRef<any, Props>((props, ref)
                             max={marks.length-1}
                             step={null}
                             marks={marks}
-                            valueLabelDisplay="on"
-                        /> */}
+                            valueLabelFormat={(x:number)=>"Bi!"+x}
+                        />
+                        <Box sx={{ m: 3 }} />
                     </Box>
                     <Typography textAlign="center" variant="subtitle2" marginBottom ={2}>Plant Cabine</Typography>
                     <MainPlaceContainer
